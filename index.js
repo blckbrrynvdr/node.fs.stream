aa4e9334598a037cb47164acfe5939b604aee299
LAPTOP-GN7979SP\v8a8r <v8a8rt@gmail.com>
2024-03-30 11:13:34 +0300

    task one done

---
+++ index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const { log } = require("console");
+
+const argv = yargs(hideBin(process.argv))
+    .option('log', {
+        alias: "l",
+        type: "string",
+        description: "путь до файла лога"
+    }).argv;
+
+const logPath = argv._[0] || 'log.txt';
+
+const randomInteger = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+const hiddenNumber = randomInteger(1, 2);
+
+const head = 1;
+const tail = 2;
+
+const getNumberName = (number) => {
+    if (number === head) {
+        return 'орёл';
+    }
+    if (number === tail) {
+        return 'решка';
+    }
+}
+
+const writeToFile = (path, data) => {
+    const writeStream = fs.createWriteStream(path);
+
+    writeStream.write(data, 'UTF8');
+    writeStream.end();
+}
+
+
+
+const writeLog = (content) => {
+    
+    console.log('zapis v log, content:', content);
+    console.log('zapis v log, path:', logPath);
+
+    let contentBuffer = '';
+    const readStream = fs.createReadStream(logPath);
+    readStream
+    .setEncoding('UTF8')
+    .on('error', (err) => {
+        if (err.code === 'ENOENT') {
+            console.log('файл не был моздаг, созхдаём');
+            writeToFile(logPath, content);
+        }
+        // console.log('read err', err);
+        readStream.close();
+    })
+    .on('data', (chank) =>{
+        contentBuffer += chank
+    })
+    .on('end', () => {
+        contentBuffer += content;
+        console.log('end', contentBuffer)
+        writeToFile(logPath, contentBuffer);
+    })
+
+}
+
+const checkNumber = (number) => {
+    number = Number(number);
+    let status;
+    if (number !== hiddenNumber) {
+        console.log('К сожалению это неправильный ответ.');
+        status = 'Поражение';
+        rl.close();
+    }
+    if (number === hiddenNumber) {
+        console.log(`Верно, это ${getNumberName(number)}!`);
+        status = 'Победа';
+        rl.close();
+    }
+
+    const logText = `Загадано число ${hiddenNumber}(${getNumberName(hiddenNumber)}).` +
+    `Пользователь ввёл ${number}(${getNumberName(number)}). Статус игры: ${status}.\n`;
+    writeLog(logText);
+}
+
+rl.question(`Добро пожаловать в игру "Орёл или Решка"!\n
+Было загадано целое число от 1 до 2, где ${head} - ${getNumberName(head)}, а ${tail} - ${getNumberName(tail)}.\n
+Введите 1 или 2 чтоб угадать, загадан орёл или решка.\n`, (answer) => {
+    checkNumber(answer);
+})
+
+rl.on('line', (input) => checkNumber(input));
